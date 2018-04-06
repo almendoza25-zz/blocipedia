cd1f65e21d0efc4b12aae515c1f0539ebbd56e7a
Elisa Mendoza <ElisaMendoza@Elisas-MacBook-Air.local>
2018-04-06 16:50:38 -0700

    Attempt 1 at User Story 4: CRUD

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,15 @@
 require_relative 'boot'
 
-require 'rails/all'
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,5 +23,8 @@ module Blocipedia
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths << File.join(config.root, "lib")
+    # Don't generate system test files.
+    config.generators.system_tests = nil
   end
 end
